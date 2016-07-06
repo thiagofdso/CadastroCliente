8ee5879988031f79c380e8e7581508cce130f4e7
Thiago Oliveira <thiagofdso.ufpa@gmail.com>
2016-07-06 10:14:53 -0300

    Refatorado codigo para o projeto codeeducation

--- App/SQL/Conexao.php
+++ App/SQL/Conexao.php
@@ -10,7 +10,7 @@
    
       public static function getInstance() {
           if (!isset(self::$instance)) {
-              self::$instance = new \PDO('mysql:host=localhost;dbname=cadcliente', 'thiagofdso', 'fernandes1', array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+              self::$instance = new \PDO('mysql:host=localhost;dbname=cadcliente', 'login', 'senha', array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
               self::$instance->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
               self::$instance->setAttribute(\PDO::ATTR_ORACLE_NULLS, \PDO::NULL_EMPTY_STRING);
           }
